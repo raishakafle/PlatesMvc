83ccd7739fd5df7b7253d889969e90f67d46ef1b
raishakafle <113068520+raishakafle@users.noreply.github.com>
2023-02-16 18:12:48 -0500

    Update

---
+++ PlatesMvc/Models/Plates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatesMvc.Models
+{
+    public class Plates
+    {
+        public int Id { get; set; }
+        public string Material { get; set; }
+
+        public int ManufacturedDate { get; set; }
+
+        public string Design { get; set; }
+        public string Size { get; set; }
+        public string Shape { get; set; }
+        public string Function { get; set; }
+
+
+
+
+    }
+
+}
